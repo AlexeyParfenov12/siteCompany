1d1ce6ef2a20b96a7d4337ef699b83628ee89fba
Parfenov Alexey <98681378+AlexeyParfenov12@users.noreply.github.com>
2026-03-21 23:33:09 +0500

    надеюсь это последний пуш

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzOPmm5gHB1BNYtNVhjovU3qmxs3ia14R-zsqbxSplxjuXoYoT82kign299ub8UTeMHQg/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyRWgOk6S_j9pyqGqLqJiwqcFE1oNqRPVaw84LOmkBUT9o8K2Q7CZfBv8AG9dei3Ma6vQ/exec";
 
 let urlParams = new URLSearchParams(window.location.search);
 let name = urlParams.get('name') || 'unknown';
