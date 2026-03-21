c8b107cbb2741e20fb9d21fe20cedaf75e69c2ab
Parfenov Alexey <98681378+AlexeyParfenov12@users.noreply.github.com>
2026-03-21 09:02:00 +0500

    add files

---
+++ script.js
@@ -0,0 +1,30 @@
+import data_ip_ok from "./data.js";
+
+let urlParams = new URLSearchParams(window.location.search);
+let name = urlParams.get('name');
+
+// Это "чистый" JS, но с использованием внешнего ресурса
+async function getUniqueUserId() {
+    try {
+        const response = await fetch('https://api.ipify.org?format=json');
+        const data = await response.json();
+        return data.ip; // Твой IP: "123.45.67.89"
+    } catch (e) {
+        return null;
+    }
+}
+
+console.log(name);
+getUniqueUserId().then(ip => {
+    data_ip_ok.forEach(item => {
+        if (item === ip) {
+            console.log(item);
+        }else{
+            data_ip_ok.push(ip);
+        }
+    });
+});
+
+data_ip_ok.forEach(item => {
+    console.log(item);
+});
