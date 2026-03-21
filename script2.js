06d161caefb06f03d084c6a0b01e6f4ed6a8879f
Parfenov Alexey <98681378+AlexeyParfenov12@users.noreply.github.com>
2026-03-21 14:16:17 +0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -19,6 +19,7 @@ button.addEventListener("click", function () {
         fetch(finalMail, { mode: "no-cors" })
           .then(() => {
             console.log("Данные отправлены в Google Таблицу!!!");
+            console.log(finalMail)
             // document.body.innerHTML = `<script> setTimeout(() => { window.close() }, 99999); </script><div style="background-color: #f05d5d; width: 100%; height: 100vh; display: flex; justify-content: center; align-items: center;"><h1 style="color: #ffffff; font-size: 24px; text-align: center; font-weight: bold; font-family: verdana, sans-serif;">Спасибо, за голос! Данные сохранены.</h1></div>`;
           })
           .catch((err) => console.error("Ошибка отправки:", err));
