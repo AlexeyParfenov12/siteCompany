8ebfe7c09ae86041768f0e88e4ec6ec16b76fc06
Parfenov Alexey <98681378+AlexeyParfenov12@users.noreply.github.com>
2026-03-26 01:15:58 +0500

    виывиы

--- script.js
+++ script.js
@@ -2,54 +2,7 @@ const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwJHeAh74-LX3
 
 let urlParams = new URLSearchParams(window.location.search);
 let name = urlParams.get('name') || 'unknown';
-// let mailValue = urlParams.get('mail') || 'unknown';
 
-
-
-// if(name === 'order'){
-//     name = 'Заказываю';
-// }else if(name === 'noOrder'){
-//     name = 'Не заказываю';
-// }else if(name === 'task'){
-//     name = 'Подумаю';
-// }
-
-
-
-// async function getUniqueUserId() {
-//     try {
-//         const response = await fetch('https://api.ipify.org?format=json');
-//         const data = await response.json();
-//         return data.ip;
-//     } catch (e) {
-//         console.error("Ошибка получения IP:", e);
-//         return null;
-//     }
-// }
-
-
-// async function startTracking() {
-//     const ip = await getUniqueUserId();
-    
-//     if (ip && mailValue) {
-//         console.log("Ваш IP:", ip);
-//         console.log("Имя:", name);
-//         console.log("Почта:", mailValue)
-
-//         // Формируем запрос к Google Таблице
-//         const finalUrl = `${GOOGLE_SCRIPT_URL}?ip=${ip}&name=${name}&mail=${mailValue}`;
-
-//         // Отправляем данные (mode: 'no-cors' нужен, чтобы не было ошибок безопасности)
-//         fetch(finalUrl, { mode: 'no-cors' })
-//             .then(() => {
-//                 console.log("Данные отправлены в Google Таблицу!!!");
-//                 document.body.innerHTML = `<img class="background-image" alt="Успешно">`;
-//             })
-//             .catch(err => console.error("Ошибка отправки:", err));
-//     }
-// }
-
-// startTracking();
 
 let button = document.querySelector(".button");
 let mail = document.querySelector(".mail");
